82c72581b74912f2d5fdbd9cab3fd2666d93aa20
qqwxe <ultrawindyyt@gmail.com>
2026-06-03 22:25:10 +0500

    Fix src/usr/bin/reminor: copy from re_minor.sh, bump to 1.0-4

---
+++ copy_reminor.py
@@ -0,0 +1,5 @@
+import shutil
+src = r'c:\Users\ultra\Downloads\протокоыл\re_minor.sh'
+dst = r'c:\Users\ultra\Downloads\протокоыл\reminor-pkg\src\usr\bin\reminor'
+shutil.copy2(src, dst)
+print('done')
