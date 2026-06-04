9ede682db0b84989aea20a91cf9d564c464933a0
qqwxe <98111882+qqwxe@users.noreply.github.com>
2026-06-04 07:54:23 +0500

    1

--- copy_reminor.py
+++
@@ -1,5 +0,0 @@
-import shutil
-src = r'c:\Users\ultra\Downloads\протокоыл\re_minor.sh'
-dst = r'c:\Users\ultra\Downloads\протокоыл\reminor-pkg\src\usr\bin\reminor'
-shutil.copy2(src, dst)
-print('done')
